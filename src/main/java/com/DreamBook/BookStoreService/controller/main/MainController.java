370943202c12bff695e703366b06c35d003a5748
jly789 <jly789@naver.com>
2023-07-07 02:41:38 +0900

    Test

--- src/main/java/com/DreamBook/BookStoreService/controller/main/MainController.java
+++ src/main/java/com/DreamBook/BookStoreService/controller/main/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/hi")
+    @GetMapping("/")
     public String main(Model model){
 
         return "main/main";
