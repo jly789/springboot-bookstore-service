32cbb05d07a5a2d34b990e2075a62717c9827d08
jly789 <jly789@naver.com>
2023-07-07 02:47:01 +0900

    commit

---
+++ src/test/java/com/DreamBook/BookStoreService/BookStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DreamBook.BookStoreService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
